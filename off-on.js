6f0b45e50c7d84fd5c0e83352895ccc34ef658ed
PelKnightingale <pelumiadesuyi@gmail.com>
2017-01-21 04:20:27 +0100

    Add files via upload
    Check your user's connectivity status and you can apply any request handling method easily.

---
+++ off-on.js
@@ -0,0 +1,8 @@
+/* Off-On v1.0.0 - Pelumi Adesuyi ; Github- PelKnightingale*/
+/*Init control variables for log*/
+/* Set Status echo-back for 1 */
+  var positive = "Online";
+  /*Set Status echo-back for 0 */
+  var negative = "Offline";
+  /* Browser connectivity query - The Magic Word */
+  var online = navigator.onLine;
